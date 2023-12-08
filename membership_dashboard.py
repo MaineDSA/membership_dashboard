51479cb7bd58efc4f1ed128868db8b97634f6339
Wil Thieme <wil.thieme@protonmail.com>
2023-12-07 19:34:43 -0500

    keep map from scrolling

--- membership_dashboard.py
+++ membership_dashboard.py
@@ -344,7 +344,7 @@ member_map = html.Div(
             id="membership_map",
 			style={
 				"display": "inline-block",
-				"height": "90vh",
+				"height": "85vh",
 				"width": "100%",
 				"padding-left": "-1em",
 				"padding-right": "-1em",
