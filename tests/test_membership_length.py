29a924b68080dfef9611335030621912158eb240
Wil Thieme <wil.thieme@protonmail.com>
2023-12-28 18:40:10 -0500

    docstrings for membership length test

--- tests/test_membership_length.py
+++ tests/test_membership_length.py
@@ -1,7 +1,8 @@
-"""Check known edge cases and possible regressions"""
+"""Perform testing to ensure membership length calculation is correct"""
 
 from ..scan_membership_lists import membership_length
 
 
 def test_membership_length():
+    """Ensure membership length calculation is correct"""
     assert membership_length("1982-06-01", list_date="2023-01-01") == 40
