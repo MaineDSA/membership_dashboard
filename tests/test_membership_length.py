03af202056ab5004dfb056b511b9233a02ab2aee
Wil Thieme <wil.thieme@protonmail.com>
2023-12-27 21:26:06 -0500

    fix indentation

--- tests/test_membership_length.py
+++ tests/test_membership_length.py
@@ -4,4 +4,4 @@ from ..scan_membership_lists import membership_length
 
 
 def test_membership_length():
-	assert membership_length("1982-06-01", list_date="2023-01-01") == 40
+    assert membership_length("1982-06-01", list_date="2023-01-01") == 40
