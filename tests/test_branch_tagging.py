1e601a2885e958db085a209e32fbec4497c08f57
Wil Thieme <wil.thieme@protonmail.com>
2023-12-31 12:00:24 -0500

    add test for proper branch tagging of +4 zips

--- tests/test_branch_tagging.py
+++ tests/test_branch_tagging.py
@@ -12,3 +12,11 @@ def test_branch_zip_tagging():
         memb_list = data_cleaning(pd.read_csv(memb_list, header=0), "2024-01-01")
         tagged_list = tagged_with_branches({"2024-01-01": memb_list}, Path("tests/test_harness_assets/fake_branch_zips.csv"))
         assert tagged_list["2024-01-01"].loc[55222]["branch"] == "Midcoast"
+
+
+def test_branch_zip_plus_four_tagging():
+    """Load a branch_zips file and attempt to apply it to test data"""
+    with open("tests/test_harness_assets/fake_membership_list_2023_late.csv") as memb_list:
+        memb_list = data_cleaning(pd.read_csv(memb_list, header=0), "2024-01-01")
+        tagged_list = tagged_with_branches({"2024-01-01": memb_list}, Path("tests/test_harness_assets/fake_branch_zips.csv"))
+        assert tagged_list["2024-01-01"].loc[255222]["branch"] == "Portland"
