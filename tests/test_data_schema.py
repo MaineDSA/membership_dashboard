4b56758c26f7d9d642a9c73bad258c003c8aab77
Wil Thieme <wil.thieme@protonmail.com>
2024-01-02 11:29:14 -0500

    update tests

--- tests/test_data_schema.py
+++ tests/test_data_schema.py
@@ -1,6 +1,5 @@
 """Validate schema of imported lists using pandera"""
 
-import os
 from glob import glob
 from ..utils.list_schema import schema
 from ..utils.scan_membership_lists import data_cleaning, scan_memb_list_from_csv
@@ -8,8 +7,8 @@ from ..utils.scan_membership_lists import data_cleaning, scan_memb_list_from_csv
 
 def test_schema():
     """Check whether imported lists match defined pandera schema"""
-    files = glob(os.path.join("tests/test/harness_assets", "fake_membership_list_*.csv"))
-    for membership_list_file in files:
-        with open(membership_list_file) as membership_csv:
-            cleaned_list = data_cleaning(scan_memb_list_from_csv(membership_csv), "2024-01-01")
-            schema(cleaned_list)
+    for membership_list_file in glob("tests/test/harness_assets/fake_membership_list_*.csv"):
+        with open(membership_list_file) as membership_csv_data:
+            scanned_list = scan_memb_list_from_csv(membership_csv_data)
+            cleaned_list = data_cleaning(scanned_list, "2024-01-01")
+            assert schema(cleaned_list)
