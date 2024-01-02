ed40f55a63e4dba83544f6f190e3d72ee2890d89
Wil Thieme <wil.thieme@protonmail.com>
2024-01-02 11:29:14 -0500

    update tests

--- tests/test_branch_tagging.py
+++ tests/test_branch_tagging.py
@@ -4,18 +4,20 @@ from pathlib import Path
 
 from ..utils.scan_membership_lists import data_cleaning, tagged_with_branches, scan_memb_list_from_csv
 
+TEST_BRANCH_ZIP_CSV = Path("tests/test_harness_assets/fake_branch_zips.csv")
+
 
 def test_branch_zip_tagging():
     """Load a branch_zips file and attempt to apply it to test data"""
     with open("tests/test_harness_assets/fake_membership_list_2023_late.csv") as memb_list:
         memb_list = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01")
-        tagged_list = tagged_with_branches({"2024-01-01": memb_list}, Path("tests/test_harness_assets/fake_branch_zips.csv"))
-        assert tagged_list["2024-01-01"].loc[55222]["branch"] == "Midcoast"
+        tagged_list = tagged_with_branches({"2024-01-01": memb_list}, TEST_BRANCH_ZIP_CSV)
+        assert tagged_list["2024-01-01"].loc[222251]["branch"] == "Central"
 
 
 def test_branch_zip_plus_four_tagging():
     """Load a branch_zips file and attempt to apply it to test data"""
     with open("tests/test_harness_assets/fake_membership_list_2023_late.csv") as memb_list:
         memb_list = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01")
-        tagged_list = tagged_with_branches({"2024-01-01": memb_list}, Path("tests/test_harness_assets/fake_branch_zips.csv"))
-        assert tagged_list["2024-01-01"].loc[255222]["branch"] == "Portland"
+        tagged_list = tagged_with_branches({"2024-01-01": memb_list}, TEST_BRANCH_ZIP_CSV)
+        assert tagged_list["2024-01-01"].loc[522481]["branch"] == "Portland"
--- tests/test_data_cleaning.py
+++ tests/test_data_cleaning.py
@@ -4,69 +4,53 @@ from ..utils.scan_membership_lists import data_cleaning, scan_memb_list_from_csv
 
 
 def test_mailing_to_unified_address_conversion():
-    """Check whether mailing address is converted to single unified address"""
+    """Check whether mailing address (late 2022 format) is converted to single unified address"""
     with open("tests/test_harness_assets/fake_membership_list_2022_late.csv") as memb_list:
-        person = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01").loc[55222]
-        assert person["address1"] == "255 Westbrook St"
-        assert person["address2"] == "Unit 2"
-        assert person["city"] == "bowdoinham"
+        person = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01").loc[222251]
+        assert person["address1"] == "PO Box 13"
+        assert person["city"] == "Turner"
         assert person["state"] == "ME"
-        assert person["zip"] == "04008"
+        assert person["zip"] == "04282-0013"
 
 
 def test_zip_code_leading_zero_padding():
-    """Check whether mailing address is converted to single unified address"""
-    with open("tests/test_harness_assets/fake_membership_list_2020_early.csv") as memb_list:
-        person = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01").loc[55222]
-        assert person["zip"] == "04011"
+    """Check whether zip codes with <4 digits are padded with leading zeros"""
+    with open("tests/test_harness_assets/fake_membership_list_2022_late.csv") as memb_list:
+        person = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01").loc[178705]
+        assert person["zip"] == "04103"
 
 
 def test_old_address_column_name_renaming():
     """Check whether old address column name (2020 era) is converted to new address column name"""
     with open("tests/test_harness_assets/fake_membership_list_2020_early.csv") as memb_list:
-        person = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01").loc[55222]
-        assert person["address1"] == "255 Westbrook St"
-        assert person["address2"] == "Unit 2"
+        person = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01").loc[222251]
+        assert person["address1"] == "PO Box 13"
+        assert person["address2"] == "Apt3"
 
 
 def test_annual_to_yearly_dues_renaming():
     """Check whether annual dues status of pre-2023 lists is converted to yearly dues status"""
     with open("tests/test_harness_assets/fake_membership_list_2022_late.csv") as memb_list:
-        person = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01").loc[55222]
-        assert person["yearly_dues_status"] == "canceled"
-
-
-def test_add_accomodations_column():
-    """Esure all members have an accomodations column"""
-    with open("tests/test_harness_assets/fake_membership_list_2020_early.csv") as memb_list:
-        person = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01").loc[55222]
-        assert person["accommodations"] is None
+        person = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01").loc[178705]
+        assert person["yearly_dues_status"] == "never"
 
 
 def test_expired_status_conversion():
     """Ensure members with expired status have this changed to lapsed"""
     with open("tests/test_harness_assets/fake_membership_list_2021_early.csv") as memb_list:
-        person = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01").loc[55222]
+        person = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01").loc[222251]
         assert person["membership_status"] == "lapsed"
 
 
 def test_lifetime_type_conversion():
     """Ensure members with expiration date of 2099 have membership_type set to lifetime"""
-    with open("tests/test_harness_assets/fake_membership_list_2023_late.csv") as memb_list:
-        person = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01").loc[155222]
+    with open("tests/test_harness_assets/fake_membership_list_2021_early.csv") as memb_list:
+        person = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01").loc[28855]
         assert person["membership_type"] == "lifetime"
-
-
-def test_accommodations_column_renaming():
-    """Ensure membership lists with misspelled accommodations column have it corrected so it can be compared"""
-    with open("tests/test_harness_assets/fake_membership_list_2022_late.csv") as memb_list:
-        memb_list = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01")
-        assert "accommodations" in memb_list.columns
-        assert "accomodations" not in memb_list.columns
 
 
 def test_membership_status_column_lowercasing():
     """Ensure membership lists with misspelled accommodations column have it corrected so it can be compared"""
-    with open("tests/test_harness_assets/fake_membership_list_2022_late.csv") as memb_list:
-        person = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01").loc[55222]
+    with open("tests/test_harness_assets/fake_membership_list_2021_early.csv") as memb_list:
+        person = data_cleaning(scan_memb_list_from_csv(memb_list), "2024-01-01").loc[222251]
         assert person["membership_status"] == "lapsed"
