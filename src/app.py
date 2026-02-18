97147219ec6d8a6172931e36a94a5172d06ea741
Ramona T <ramona.t.me@pm.me>
2026-02-18 16:38:41 -0500

    fix favicon loading

--- src/app.py
+++ src/app.py
@@ -3,14 +3,21 @@ import dash_bootstrap_components as dbc
 import dash_bootstrap_templates
 from dash import Dash, Input, Output, clientside_callback, html
 
+FAVICON = {
+    "rel": "icon",
+    "href": "/assets/favicon.svg",
+    "type": "image/svg+xml",
+}
 DBC_CSS = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
+EXTERNAL_STYLESHEETS = [dbc.themes.DARKLY, dbc.themes.JOURNAL, dbc.icons.FONT_AWESOME, FAVICON, DBC_CSS]
 TEMPLATES = ["darkly", "journal"]
-EXTERNAL_STYLESHEETS = [dbc.themes.DARKLY, dbc.themes.JOURNAL, DBC_CSS, dbc.icons.FONT_AWESOME]
 
 app = Dash(
     external_stylesheets=EXTERNAL_STYLESHEETS,
     # these meta_tags ensure content is scaled correctly on different devices
-    meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
+    meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"},
+    ],
     suppress_callback_exceptions=True,
     use_pages=True,
 )
