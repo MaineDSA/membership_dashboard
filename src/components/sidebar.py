80e541dddd476adb3c5fed4f3cf7a2284c312754
Ramona T <ramona.t.me@pm.me>
2026-07-18 19:21:17 -0400

    Restyle sidebar

--- src/components/sidebar.py
+++ src/components/sidebar.py
@@ -11,85 +11,114 @@ member_list_keys = list(scan_lists.MEMB_LISTS.keys())
 
 def sidebar(*, compare: bool = True) -> html.Div:
     return html.Div(
+        style={
+            "display": "flex",
+            "flexDirection": "column",
+            "height": "100vh",
+            "padding-left": "0rem",
+            "padding-bottom": "1rem",
+        },
         children=[
-            dbc.Row(
-                [
-                    dbc.Col(
-                        html.Img(
-                            src="/assets/favicon.svg",
-                            alt="Red Maine DSA logo of a moose holding a rose in its mouth under the text Maine DSA",
-                        ),
-                        align="center",
-                    ),
-                    dbc.Col(
-                        [
-                            dbc.Row(
-                                [
-                                    dbc.Col(dbc.Label(className="fa fa-sun", html_for="color-mode-switch")),
-                                    dbc.Col(
-                                        dbc.Switch(
-                                            id="color-mode-switch",
-                                            value=True,
-                                            className="d-inline-block ms-1",
-                                            persistence=True,
-                                        )
-                                    ),
-                                    dbc.Col(dbc.Label(className="fa fa-moon", html_for="color-mode-switch")),
-                                ],
-                                className="g-0",
-                            )
+            html.Div(
+                style={
+                    "flex": "1 1 auto",
+                    "overflow-x": "clip",
+                    "overflow-y": "auto",
+                },
+                children=[
+                    dbc.Row(
+                        children=[
+                            dbc.Col(
+                                html.Img(
+                                    src="/assets/favicon.svg",
+                                    alt="Red logo of a moose holding a rose in its mouth under the text Maine DSA",
+                                ),
+                                align="center",
+                            ),
+                            dbc.Col(
+                                dbc.Row(
+                                    children=[
+                                        dbc.Col(dbc.Label(className="fa fa-sun", html_for="color-mode-switch")),
+                                        dbc.Col(
+                                            dbc.Switch(
+                                                id="color-mode-switch",
+                                                value=True,
+                                                className="d-inline-block ms-1",
+                                                persistence=True,
+                                            )
+                                        ),
+                                        dbc.Col(dbc.Label(className="fa fa-moon", html_for="color-mode-switch")),
+                                    ],
+                                    className="g-0",
+                                ),
+                                width="auto",
+                                align="center",
+                            ),
                         ],
-                        width="auto",
-                        align="center",
+                        id="header",
+                    ),
+                    dbc.Row(
+                        children=[
+                            dbc.Col(
+                                children=[
+                                    html.Hr(),
+                                    html.Div(
+                                        html.P("Membership Dashboard", className="lead"),
+                                        id="title",
+                                    ),
+                                    dbc.Nav(
+                                        children=[
+                                            dbc.NavLink(
+                                                html.Div(page["name"], className="ms-2"),
+                                                href=page["path"],
+                                                active="exact",
+                                            )
+                                            for page in dash.page_registry.values()
+                                        ],
+                                        id="navigation",
+                                        vertical=True,
+                                        pills=True,
+                                    ),
+                                ]
+                            ),
+                        ],
+                    ),
+                ],
+            ),
+            html.Div(
+                children=[
+                    html.Hr(),
+                    html.Div(
+                        children=[
+                            html.Div(
+                                children=[
+                                    dcc.Dropdown(
+                                        options=member_list_keys,
+                                        value=member_list_keys[0],
+                                        id="list-selected",
+                                        persistence=True,
+                                        persistence_type="session",
+                                    ),
+                                    html.Div(html.P("Active List")),
+                                ],
+                                id="list-selector",
+                            ),
+                            html.Div(
+                                style={"display": "none" if not compare else "block"},
+                                children=[
+                                    dcc.Dropdown(
+                                        id="list-compare",
+                                        persistence=True,
+                                        persistence_type="session",
+                                    ),
+                                    html.Div(html.P("Compare To")),
+                                ],
+                                id="list-selector-compare",
+                            ),
+                        ],
+                        id="list-selectors",
                     ),
                 ]
-            ),
-            dbc.Row(
-                dbc.Col(
-                    [
-                        html.Div(
-                            [
-                                html.Hr(),
-                                html.P("Membership Dashboard", className="lead"),
-                            ],
-                        ),
-                        html.Div(
-                            [
-                                dcc.Dropdown(
-                                    options=member_list_keys,
-                                    value=member_list_keys[0],
-                                    id="list-selected",
-                                    persistence=True,
-                                    persistence_type="session",
-                                ),
-                                html.Div([html.P("Active List")]),
-                            ],
-                        ),
-                        html.Div(
-                            [
-                                dcc.Dropdown(
-                                    id="list-compare",  # Options are now handled by the callback below
-                                    persistence=True,
-                                    persistence_type="session",
-                                ),
-                                html.Div([html.P("Compare To")]),
-                            ],
-                            style={"display": "none" if not compare else "block"},
-                        ),
-                        dbc.Nav(
-                            [
-                                dbc.NavLink(
-                                    html.Div(page["name"], className="ms-2"),
-                                    href=page["path"],
-                                    active="exact",
-                                )
-                                for page in dash.page_registry.values()
-                            ],
-                            vertical=True,
-                            pills=True,
-                        ),
-                    ]
-                ),
             ),
         ],
     )
